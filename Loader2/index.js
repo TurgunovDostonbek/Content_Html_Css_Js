de6298eb5ac674d1a44cc1b33c73ee071ea95d9f
TurgunovDostonbek <turgunovdostonbek@gmail.com>
2024-09-10 08:48:49 +0500

    Loader

--- Loader2/index.js
+++ Loader2/index.js
@@ -1,29 +1,31 @@
-// const card = document.querySelector(".card");
-// const loader = document.querySelector(".loader");
-// const btn = document.getElementById("btn");
+const card = document.querySelector(".card");
+const loader = document.querySelector(".loader");
+const btn = document.getElementById("btn");
 
-// card.addEventListener("click", () => {
-//   btn.classList.add("hidden");
-//   loader.classList.remove("add");
-//   setInterval(() => {
-//     btn.classList.remove("hidden");
-//     loader.classList.add("add");
-//   }, 2000);
-// });
+card.addEventListener("click", () => {
+  btn.classList.add("hidden");
+  loader.classList.remove("add");
+  setInterval(() => {
+    btn.classList.remove("hidden");
+    loader.classList.add("add");
+  }, 2000);
+});
 
 const form = document.getElementById("Form");
 const h1 = document.querySelector(".h1");
-form.addEventListener("click", (e) => {
+
+form.addEventListener("submit", (e) => {
   e.preventDefault();
   const input1 = form.input1.value;
   const input2 = form.input2.value;
   const sum = input1 * input2;
+  Number(sum);
   if (sum !== Number) {
     h1.textContent = "Xatooooo";
   } else {
     h1.textContent = sum;
   }
 
-  console.log(input1);
-  console.log(input2);
+  console.log(typeof sum);
+  console.log(typeof input2);
 });
